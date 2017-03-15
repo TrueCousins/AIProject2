24fbe45212beb00f7c3739d0d8892ce895b14405
Colleen Cousins <ctc130030@utdallas.edu>
2017-03-14 23:55:32 -0500

    Update AI2.java
    added priority queue and stack

--- AI2.java
+++ AI2.java
@@ -9,54 +9,123 @@ import java.io.*;
 public class AI2 {
   public static void main(String[] args) {
 
-      Boolean fc = false;
+    Boolean fc = false;
 
-      File varF = new File(args[0]);
-      File conF = new File(args[1]);
+    File varF = new File(args[0]);
+    File conF = new File(args[1]);
 
-      Scanner vScan = null;
-      Scanner cScan = null;
-      Map<String, List<Integer>> vList = new HashMap<String, List<Integer>>();
+    Scanner vScan = null;
+    Scanner cScan = null;
+    Map<String, Set<Integer>> vList = new HashMap<String, Set<Integer>>();
+
+    List<char[]> constr = new ArrayList<>();
+
+    try {
+      vScan = new Scanner(varF);
+
+      while(vScan.hasNext()) {
+        String key = vScan.next();
+        Set<Integer> tempList = new HashSet<>();
+
+        while(vScan.hasNextInt())
+          tempList.add(vScan.nextInt());
+
+          vList.put(key.substring(0, 1), tempList);
+      } //end while
+
+        /*for(Map.Entry entry : vList.entrySet()) {
+          System.out.println(entry.getKey() + ", " + entry.getValue());
+        } //end for */
+    } //end try
+    catch(FileNotFoundException g) {
+      g.printStackTrace();
+    } //end catch
+
+    try { //con file parsing
+      cScan = new Scanner(conF);
+      while(cScan.hasNextLine()) {
+        char in[] = cScan.nextLine().toCharArray();
+        char arr[] = {in[0], in[2], in[4]};
+        constr.add(arr);
+      } //end while
+        //for(int i = 0; i < constr.size(); i++) {
+          //char[] got = constr.get(i);
+          //System.out.println(got[0] + " " + got[1] + " " + got[2]);
+        //} //end for
+    } //end try
+    catch(FileNotFoundException f) {
+      f.printStackTrace();
+    } //end catch
+
+    if(args[2].equals("fc"))
+      fc = true;
+
+    //LinkedList<Node> space = new LinkedList<Node>();
+    Node head = new Node(vList);
+    //space.addFirst(head);
+
+    Node next = head;
+    
+    Stack<Node> search = new Stack<>();
+    Comparator<Node> comparator = new CSPCompare();
+    PriorityQueue<Node> pq = new PriorityQueue<>(500, comparator);
+    
+    
+ 
+ 	
+	int i = 0;
+    while(i < 30) {
+    	char nextVariable = next.varHuer(constr);
+    	Node child = null;
+		if(vList.containsKey(nextVariable))
+		{
+			List<Integer> tempList = new ArrayList<Integer>();
+    		tempList = (List<Integer>) vList.get(nextVariable);
+			for(int j = 0 ; j < vList.get(nextVariable).size();j++)
+			{
+				String var = Character.toString(nextVariable);
+	    		child = new Node(next, var, tempList.get(i));
+	    		child.valHuer(constr);
+	    		pq.offer(child);
+			}
+			// Add next Node to stack
+		 	search.push(next);
+		 	
+		}
+		
+		boolean flag = false;
+		
+		while(!pq.isEmpty())
+		{
+			child = pq.poll();
+		 	i++;
+		 	if(child.constrCheck() ==  true)
+		 	{
+		 		next = child;
+		 		flag = true;
+		 		pq.clear();
+		 		break;
+		 	}
+		 	else 
+		 	{
+		 		//i++;
+		 		child.printAssignment();
+		 		//child = pq.poll();
+		 	}
+		}
+		
+		if(flag == false)
+		{
+			if(!search.isEmpty()){
+				next = search.pop();
+				System.out.println("There is no solution.");
+				return;
+			}
+
+		}
+    	//next = new Node(head);
+
       
-      try { //var file parsing
-    	  vScan = new Scanner(varF);
-    	  
-    	  //String[] parts = list.split(":");
-          while(vScan.hasNext())
-          {
-        	  String key = vScan.next();
-        	  List<Integer> tempList = new ArrayList<Integer>();
-        	  //System.out.println("KEY: " + key);
-        	  
-        	  
-        	  while(vScan.hasNextInt())
-        		  tempList.add(vScan.nextInt());
-        	    	  
-        	  //System.out.println("VALUE: " + tempList);
-        	  vList.put(key.substring(0, 1), tempList);
-          }  
-          
-          // Check map
-          for (Map.Entry entry : vList.entrySet()) {
-        	    System.out.println(entry.getKey() + ", " + entry.getValue());
-        	}
-
-          vScan.close();
-      } //end try
-      catch(FileNotFoundException e) {
-        e.printStackTrace();
-      } //end catch
-
-      try { //con file parsing
-
-      } //end try
-      catch(FileNotFoundException f) {
-        f.printStackTrace();
-      } //end catch
-
-
-      if(args[2].equals("fc"))
-        fc = true;
-
+    } //end for
   } //end main
 } //end class AI2
